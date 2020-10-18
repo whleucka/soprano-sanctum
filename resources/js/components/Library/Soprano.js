856e576d7e7188dc1d0ad36cd272fd8b5e115fc2
Will Hleucka <william.hleucka@gmail.com>
2020-10-18 10:07:36 -0600

    Refactor directory module

---
+++ resources/js/components/Library/Soprano.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const Soprano = {
+    getUser: async function () {
+        const response = await axios.get("/api/user", {
+            withCredentials: true,
+        });
+        return response.data;
+    },
+    getDirectories: async function () {
+        const response = await axios.get("/api/directory", {
+            withCredentials: true,
+        });
+        return response.data;
+    },
+};
