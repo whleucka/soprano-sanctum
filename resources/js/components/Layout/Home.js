979a4ed207a2723665c3e7c96ea765ec398f17d8
Will Hleucka <william.hleucka@gmail.com>
2020-11-02 12:53:13 -0700

    Now Playing instead of confusing Current Playlist

--- resources/js/components/Layout/Home.js
+++ resources/js/components/Layout/Home.js
@@ -4,7 +4,7 @@ import PlaylistModule from "../Module/PlaylistModule";
 const Home = () => {
     return (
         <section id="home" className="content">
-            <h1>Current Playlist</h1>
+            <h1>Now Playing</h1>
             <PlaylistModule />
         </section>
     );
